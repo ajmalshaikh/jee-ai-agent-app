83760c1543f0029f4f01b5ff22624e7d29407233
ajmalshaikh <ajmalmustafa@gmail.com>
2026-04-19 23:37:20 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,17 +1,7 @@
 from fastapi import FastAPI
-from agents.tutor import tutor_agent
-from agents.quiz import quiz_agent
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "App is running"}
-
-@app.get("/tutor")
-def tutor(topic: str):
-    return {"response": tutor_agent(topic)}
-
-@app.get("/quiz")
-def quiz(topic: str):
-    return {"response": quiz_agent(topic)}
+    return {"message": "working"}
